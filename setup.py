59de5213d0392b12d03be024460a602029e7daaf
saisumith321 <79842398+saisumith321@users.noreply.github.com>
2025-08-20 22:52:49 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='my_python_project',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # Your project's dependencies would go here.
+        # For this example, we'll keep it simple.
+    ],
+    # You can add more metadata here, like author, description, etc.
+)
